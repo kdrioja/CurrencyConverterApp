e720deaa74e1488f421b0dd1e49715245534f52e
Kenia Rioja <kdrioja@gmail.com>
2019-08-16 17:04:27 -0700

    Completed onConvertButton

--- app/src/main/java/com/example/currencyconverterapp/MainActivity.java
+++ app/src/main/java/com/example/currencyconverterapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.currencyconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Trace;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onConvertButton(View view) {
+        EditText pounds = (EditText) findViewById(R.id.numberEditText);
+        double dollars = Double.valueOf(pounds.getText().toString()) * 1.22;
+        Toast.makeText(this, "$" + dollars, Toast.LENGTH_LONG);
     }
 }
